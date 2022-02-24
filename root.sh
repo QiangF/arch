3fb2e1de8c6fb20f04ced0b2f238ba2aed2eac9e
lotw69 <53122963+lotw69@users.noreply.github.com>
2022-02-24 12:33:05 -0800

    Add files via upload

--- root.sh
+++ root.sh
@@ -9,10 +9,10 @@ function PACMAN_KEYS() {
   clear
   dialog --infobox "Fixing The Pacman (Repos) Keys." 3 35
   sleep 2
-  sudo pacman-key --init
-  sudo pacman-key --populate archlinux
-  sudo reflector --country US --latest 10 --sort rate --verbose --save /etc/pacman.d/mirrorlist
-  sudo pacman -Sy
+   pacman-key --init
+   pacman-key --populate archlinux
+   reflector --country US --latest 10 --sort rate --verbose --save /etc/pacman.d/mirrorlist
+   pacman -Sy
 }
 
 ### CLI Programs                                                             ###
@@ -20,7 +20,7 @@ function CLI_PROGS() {
     clear
     dialog --infobox "Installing some CLI programs." 3 32
     sleep 2
-    sudo pacman -S --noconfirm --needed base-devel nano networkmanager man-db man-pages git btrfs-progs systemd-swap xfsprogs reiserfsprogs jfsutils nilfs-utils terminus-font ntp dialog neofetch git wget rsync htop openssh archlinux-wallpaper glances python-defusedxml python-packaging bashtop packagekit reflector bat mc lynx bwm-ng lsd gtop bpytop duf ncdu
+     pacman -S --noconfirm --needed base-devel nano networkmanager man-db man-pages git btrfs-progs systemd-swap xfsprogs reiserfsprogs jfsutils nilfs-utils terminus-font ntp dialog neofetch git wget rsync htop openssh archlinux-wallpaper glances python-defusedxml python-packaging bashtop packagekit reflector bat mc lynx bwm-ng lsd gtop bpytop duf ncdu
 }
 
 ### Set Number Of CPUs In MAKEFLAGS                                          ###
@@ -34,37 +34,37 @@ function MAKEFLAGS_CPU() {
       16)
           echo "You have " $numberofcores" cores."
           echo "Changing the makeflags for "$numberofcores" cores."
-          sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j16"/g' /etc/makepkg.conf
+           sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j16"/g' /etc/makepkg.conf
           echo "Changing the compression settings for "$numberofcores" cores."
-          sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 16 -z -)/g' /etc/makepkg.conf
+           sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 16 -z -)/g' /etc/makepkg.conf
           ;;
       8)
           echo "You have " $numberofcores" cores."
           echo "Changing the makeflags for "$numberofcores" cores."
-          sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j8"/g' /etc/makepkg.conf
+           sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j8"/g' /etc/makepkg.conf
           echo "Changing the compression settings for "$numberofcores" cores."
-          sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 8 -z -)/g' /etc/makepkg.conf
+           sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 8 -z -)/g' /etc/makepkg.conf
           ;;
       6)
           echo "You have " $numberofcores" cores."
           echo "Changing the makeflags for "$numberofcores" cores."
-          sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j8"/g' /etc/makepkg.conf
+           sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j8"/g' /etc/makepkg.conf
           echo "Changing the compression settings for "$numberofcores" cores."
-          sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 6 -z -)/g' /etc/makepkg.conf
+           sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 6 -z -)/g' /etc/makepkg.conf
           ;;
       4)
           echo "You have " $numberofcores" cores."
           echo "Changing the makeflags for "$numberofcores" cores."
-          sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j4"/g' /etc/makepkg.conf
+           sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j4"/g' /etc/makepkg.conf
           echo "Changing the compression settings for "$numberofcores" cores."
-          sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 4 -z -)/g' /etc/makepkg.conf
+           sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 4 -z -)/g' /etc/makepkg.conf
           ;;
       2)
           echo "You have " $numberofcores" cores."
           echo "Changing the makeflags for "$numberofcores" cores."
-          sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j2"/g' /etc/makepkg.conf
+           sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j2"/g' /etc/makepkg.conf
           echo "Changing the compression settings for "$numberofcores" cores."
-          sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 2 -z -)/g' /etc/makepkg.conf
+           sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 2 -z -)/g' /etc/makepkg.conf
           ;;
       *)
           echo "We do not know how many cores you have."
@@ -76,7 +76,7 @@ function MAKEFLAGS_CPU() {
 
 
 ### Main Program                                                             ###
-sudo pacman -S --noconfirm --needed dialog
+ pacman -S --noconfirm --needed dialog
 PACMAN_KEYS
 MAKEFLAGS_CPU
 CLI_PROGS
