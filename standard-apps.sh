4a58864d0dca73ee1d4e67f06445896b55513d82
lotw69 <53122963+lotw69@users.noreply.github.com>
2022-05-04 22:00:52 -0700

    Add files via upload

--- standard-apps.sh
+++ standard-apps.sh
@@ -6,15 +6,37 @@
 ### Review and edit before using                                             ###
 ################################################################################
 
+### Which AUR Helper did you install?                                        ###
+function AUR_HELPER() {
+  clear
+  echo "################################################################################"
+  echo "### Which AUR Helper Did You Install?                                        ###"
+  echo "### 1)  YAY                                                                  ###"
+  echo "### 2)  PARU                                                                 ###"
+  echo "################################################################################"
+  read case;
+
+  case $case in
+    1)
+    ZB="yay"
+    ;;
+    2)
+    ZB="paru"
+    ;;
+  esac
+}
+
 ### Software To Install (My Standard Applications)                           ###
 function INSTALL_STDSOFTWARE() {
   clear
   dialog --infobox "Installing Standard Software." 3 33
   sleep 2
   sudo pacman -S --noconfirm --needed firefox winetricks playonlinux steam handbrake obs-studio gimp libreoffice-fresh clementine kdenlive aspell-en youtube-dl paperwork terminator
+  $ZB system-monitoring-center
   #sudo systemctl enable tuned.service
 }
 
 ### Main Program                                                             ###
 clear
+AUR_HELPER
 INSTALL_STDSOFTWARE
