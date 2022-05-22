c174c15cf22edaf535a2b52b4866b62fcf516b66
lotw69 <53122963+lotw69@users.noreply.github.com>
2022-05-22 15:20:17 -0700

    Add files via upload

--- standard-apps.sh
+++ standard-apps.sh
@@ -32,7 +32,7 @@ function INSTALL_STDSOFTWARE() {
   dialog --infobox "Installing Standard Software." 3 33
   sleep 2
   sudo pacman -S --noconfirm --needed firefox winetricks steam handbrake obs-studio gimp libreoffice-fresh clementine kdenlive aspell-en youtube-dl paperwork terminator
-  $ZB -S --noconfirm -needed system-monitoring-center playonlinux
+  $ZB -S --noconfirm -needed system-monitoring-center
   #sudo systemctl enable tuned.service
 }
 
